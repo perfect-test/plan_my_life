b2032b1c8ef8bd117ec202276b2bfba3c7711e5a
mike-sid <mike-sid@yandex-team.ru>
2015-09-02 15:03:23 +0300

    added first prototype version

---
+++ src/main/webapp/scripts/calendar.js
@@ -0,0 +1,14 @@
+CALENDAR = {
+
+
+
+    getCalendarData: function () {
+        return $.getJSON("test-sources/calendar-data.json");
+    },
+
+    initCalendarBlock: function () {
+        var calendarBlock = $("#calendar-block");
+
+    }
+
+};
